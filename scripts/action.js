d0d555d7efa1405c3c87cc118149bec8d994bd7a
Valentin Levalet <valentin.levalet@live.fr>
2016-02-28 22:11:55 +0100

    Division des sources json pour réduire la mémoire nécessaire sur le téléphone, Ajout de nouvelles règles handlebars, Ajustement style pour le mobile

--- scripts/action.js
+++ scripts/action.js
@@ -1,10 +1,13 @@
+//$("#champsSaisie").focus();
+
 function annuler(){
     document.getElementById('champsSaisie').value = "";
+    $("#champsSaisie").focus();
 }
 
 function cacheExemples(){
-    var selec = document.getElementsByClassName('example');
+    var selec = document.getElementsByClassName('aff');
     for(var i = 0; i < selec.length; i++){
         selec[i].classList.toggle('cacher');
-    }
+    };
 }
